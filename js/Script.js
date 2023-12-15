a60936284ad37c83b15b16a20acc14566ddf63f6
kapy0312 <kapy0312@gmail.com>
2023-12-15 11:36:17 +0800

    20231215_v3

--- js/Script.js
+++ js/Script.js
@@ -15,18 +15,31 @@ $('#co-submit').on('click', function () {
     // Email_Name = "RequestArray[3]";
     // Email_Address = "kapy0312@gmail.com"
 
-    $.ajax({
-        url: google_apps_script_url,
-        type: 'POST',
-        dataType: 'text',
-        data: JSON.stringify({ DataArray: DataArray }),
-        contentType: 'text/plain; charset=utf-8',
-        success: function (response) {
-            alert(response); // 显示成功或错误消息
-        },
-        error: function () {
-            alert('Request Failed'); // 处理错误情况
-        }
-    });
-
+    // 檢查任意一個 val() 是否為空
+    if (DataArray[1] === '' || DataArray[2] === '' || DataArray[3] === '' || DataArray[4] === '') {
+        // 如果任意一個為空，執行相應的條件
+        //console.log('請填寫完整資訊');
+        alert('請填寫完整資訊'); // 处理错误情况
+        // 在這裡可以加上其他處理空值的程式碼
+    } else {
+        // 如果都不為空，執行其他操作
+        $.ajax({
+            url: google_apps_script_url,
+            type: 'POST',
+            dataType: 'text',
+            data: JSON.stringify({ DataArray: DataArray }),
+            contentType: 'text/plain; charset=utf-8',
+            success: function (response) {
+                alert(response); // 显示成功或错误消息
+                
+                $('#co-name').val('');
+                $('#co-email').val('');
+                $('#co-subject').val('');
+                $('#co-message').val('');
+            },
+            error: function () {
+                alert('Request Failed'); // 处理错误情况
+            }
+        });
+    }
 });
